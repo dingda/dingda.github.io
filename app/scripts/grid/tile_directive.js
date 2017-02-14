e90f13165bf98aa2c3ce0b6406c92afcc6abd4c8
dingda6 <dingda6@gmail.com>
2017-02-14 15:15:33 -0800

    2048 test

---
+++ app/scripts/grid/tile_directive.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+angular.module('Grid')
+.directive('tile', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      ngModel: '='
+    },
+    templateUrl: 'scripts/grid/tile.html'
+  };
+});
